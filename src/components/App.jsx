a17fc816e422e50aad8157b1ecf51c9addae8f58
Дмитро <129288908+Kreker00@users.noreply.github.com>
2024-01-04 19:32:40 +0200

    df

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { ContactEntryForm } from './ContactEntryForm/ContactEntryForm';
+import { ContactList } from './ContactList/ContactList';
+import { SearchFilter } from './SearchFilter/SearchFilter';
+import { GlobalStyle } from './GlobalStyle';
+import { fetchContacts } from 'redux/operations';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { selectIsLoading, selectError } from '../redux/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <ContactEntryForm />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <SearchFilter />
+      <ContactList />
+      <GlobalStyle />
+    </>
   );
 };
